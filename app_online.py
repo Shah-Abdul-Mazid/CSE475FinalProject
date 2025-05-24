bdf940e3468bfa5292bb9f02472e3435a5be43c7
Shah-Abdul-Mazid <ezanshah58@gmail.com>
2025-05-25 01:45:47 +0600

    Project

--- app_online.py
+++ app_online.py
@@ -23,7 +23,7 @@ from logging.handlers import RotatingFileHandler
 import plotly.express as px
 from yolo_cam.eigen_cam import EigenCAM
 from yolo_cam.utils.image import scale_cam_image, show_cam_on_image
-from streamlit_webrtc import  webrtc_streamer
+from streamlit_webrtc import  webrtc_streamer,WebRtcMode,RTCConfiguration
 
 # Setup logging
 handler = RotatingFileHandler("app.log", maxBytes=10*1024*1024, backupCount=5)
